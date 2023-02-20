9f55fc2f178fae66a2fca9f461a63f94dd3f2e2b
Niki Hapunik <nikitahapunik@gmail.com>
2023-02-20 21:43:36 +0000

    ranomize lesson or avice feedback

--- message-generator.js
+++ message-generator.js
@@ -1,22 +1,26 @@
 const olderGeneration = [['Hanna', 86], ['Mike', 90], ['Paul', 88], ['Jen', 77], ['Liam', 79]];
 
-const adviceArray = ['Draw inspiration from all the people you meet.', 'Find mentors who can guide you and challenge you.', 'Make most of less.', 'lead your life.','Be more thankful and less self-centered.'];
+const adviceArray = ['draw inspiration from all the people you meet.', 'find mentors who can guide you and challenge you.', 'make most of less.', 'lead your life.','be more thankful and less self-centered.'];
 
-const lessonArray = ['If you get your priorities right, you\'ll be happier!', 'Make decisions, nobody can make them for you.', 'Peace of mind is more important than any material possesions could ever be.', 'Never choke a hammer, always hold it by the end of the handle.', 'Baby, be a simple kind of man...'];
+const lessonArray = ['if you get your priorities right, you\'ll be happier!', 'make decisions, nobody can make them for you.', 'peace of mind is more important than any material possesions could ever be.', 'never choke a hammer, always hold it by the end of the handle.', 'baby, be a simple kind of man...'];
 
 const randomizePerson = () => {
     const randomNum = Math.floor(Math.random() * 5);
-    return `${olderGeneration[randomNum][0]}, aged ${olderGeneration[randomNum][1]}, gives this advice:`
+    return `${olderGeneration[randomNum][0]}, aged ${olderGeneration[randomNum][1]}`
 };
 
 const randomAdvice = () => {
     const randomNum2 = Math.floor(Math.random() * 5);
-    return adviceArray[randomNum2];
+    return " gives this life advice - " + adviceArray[randomNum2];
 }
-const randomizeLesson = () => {
+const randomLesson = () => {
     const randomNum = Math.floor(Math.random() * 5);
-    return 'The lesson is: ' + lessonArray[randomNum];
+    return ' learned this throughout the years: ' + lessonArray[randomNum];
 } 
 
+const lessonOrAdvice = () => {
+    const coinFlip = Math.floor(Math.random() * 2);
+    return coinFlip === 1 ? randomAdvice() : randomLesson();
+}
 // Display random (potentially silly) advice quote
-console.log(`${randomizePerson()} ${randomAdvice()} ${randomizeLesson()}`);
+console.log(randomizePerson() + lessonOrAdvice());
